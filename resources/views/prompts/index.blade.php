a4498f098b02afd66d3e7229b1dd40cf241f7ad3
Rafe Khatebi <rkhatibi2003@gmail.com>
2026-03-01 06:57:53 +0430

    Updated the prompt page(index) added more bootstrap-5 classes

--- resources/views/prompts/index.blade.php
+++ resources/views/prompts/index.blade.php
@@ -1,37 +1,47 @@
 <x-app-layout>
     <div class="container mx-auto p-6">
-        <div class="flex items-center  justify-between mb-6">
-            {{-- items-center justify-between mb-6 --}}
-            <h1 class="text-2xl font-bold">Prompts</h1>
-            <a href="{{ route('prompts.create') }}" class="btn btn-primary">Create New Prompt</a>
+        <div class="col-md-12">
+            <div class="flex items-center  justify-between mb-6">
+                {{-- items-center justify-between mb-6 --}}
+                <h1 class="text-2xl font-bold">Prompts</h1>
+                <a href="{{ route('prompts.create') }}" class="btn btn-primary">Create New Prompt</a>
+            </div>
         </div>
         @foreach ($prompts as $prompt)
             <div class="card w-full bg-dark-200 shadow-xl mb-4">
                 <div class="card-body">
-                    <p>#{{ $prompt->id }}</p>
-                    <h2 class="card-title">{{ $prompt->title }}</h2>
-                    <p>{{ $prompt->content }}</p>
-                    <p class="text-sm text-gray-200">Last edited: {{ $prompt->updated_at->diffForHumans() }}</p>
-                    <p>User: {{ $prompt->user->name }}</p>
-                    @if ($prompt->tags)
-                        <div class="badge badge-outline">{{ $prompt->tags }}</div>
-                        <div class="row flex items-center justify-content-between ">
-                            <p>index: {{ $loop->index + 1 }}</p>
-                            <div class="col-md-6"></div>
-                            <div class="col-md-6"><a href="{{ route('prompts.edit', $prompt) }}"
-                                    class="btn btn-sm btn-secondary">Edit</a>
-                                <form action="{{ route('prompts.destroy', $prompt) }}" method="POST"
-                                    onsubmit="return confirm('Are you sure you want to delete this prompt?')">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-sm btn-error text-white">
-                                        Delete
-                                    </button>
-                                </form>
-                                <a href="{{ route('prompts.show', $prompt) }}" class="btn btn-sm btn-info">View</a>
+                    <div class="row">
+                        <div class="col-md-8">
+                            <p>#{{ $prompt->id }}</p>
+                            <h2 class="card-title">{{ $prompt->title }}</h2>
+                            <p>{{ $prompt->content }}</p>
+                            <p class="text-sm text-gray-200">Last edited: {{ $prompt->updated_at->diffForHumans() }}</p>
+                            <p>User: {{ $prompt->user->name }}</p>
+                            @if ($prompt->tags)
+                                <div class="badge badge-outline">{{ $prompt->tags }}</div>
+                            @endif
+                        </div>
+                        <div class="col-md-4">
+                            <div class="row">
+                                <div class="col-md-2"> <a
+                                        href="{{ route('prompts.edit', $prompt) }}"class="btn btn-sm btn-secondary">Edit</a>
+                                </div>
+                                <div class="col-md-2">
+                                    <form action="{{ route('prompts.destroy', $prompt) }}" method="POST"
+                                        onsubmit="return confirm('Are you sure you want to delete this prompt?')">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-sm btn-danger text-white">
+                                            Delete
+                                        </button>
+                                    </form>
+                                </div>
+                                <div class="col-md-2">
+                                    <a href="{{ route('prompts.show', $prompt) }}" class="btn btn-sm btn-info">View</a>
+                                </div>
                             </div>
                         </div>
-                    @endif
+                    </div>
                 </div>
             </div>
         @endforeach
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,13 @@
-<nav class="flex items-center justify-end gap-4">
-    @auth
-        <a href="{{ route('dashboard') }}" class="px-5 py-2 border rounded-sm btn btn-md">Dashboard</a>
-    @else
-        <a href="{{ route('login') }}" class="px-5 py-1.5  btn btn-md p-3">Log in</a>
-        @if (Route::has('register'))
-            <a href="{{ route('register') }}" class="px-5 py-2  btn btn-md">Register</a>
-        @endif
-    @endauth
-</nav>
+<x-app-layout>
+
+    <nav class="flex items-center justify-end gap-4">
+        @auth
+            <a href="{{ route('dashboard') }}" class="px-5 py-2 border rounded-sm btn btn-md">Dashboard</a>
+        @else
+            <a href="{{ route('login') }}" class="px-5 py-1.5  btn btn-md p-3">Log in</a>
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}" class="px-5 py-2  btn btn-md">Register</a>
+            @endif
+        @endauth
+    </nav>
+</x-app-layout>
